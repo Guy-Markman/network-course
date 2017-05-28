ded2ad4eab69d8c1a6e92f96dd1db2db7b3598f5
Alon Bar-Lev <alon.barlev@gmail.com>
2017-05-29 02:27:37 +0300

    multicast_chat: add javascript documentation

---
+++ multicast_chat/html/test.js
@@ -0,0 +1,19 @@
+/**
+ * @file html/test.js Implementation of @ref front_end
+ * @defgroup front_end Frontend implementation
+ * @addtogroup front_end
+ * @{
+ */
+
+/**
+ * Test javascript.
+ * @param a (string) parameter1.
+ * @param b (string) parameter2.
+ * @returns null.
+ */
+function test1(a, b) {
+	alert("ok " + a + " " + b);
+	return null;
+}
+
+/** @} */
